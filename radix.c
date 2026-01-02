8f8af5b6abae5bccf436d1d4ed336294d8d1f181
Kevin <kevinpremkumar23@gmail.com>
2026-01-03 00:59:56 +0530

    Completed radix.c initial prototype

--- radix.c
+++ radix.c
@@ -1,6 +1,15 @@
 /**
  *      Number Systems
  *      Utilities for conversions among number systems
+ * 
+ *      ___Working__Procedure___
+ *      ->  User can utilize functions with command line syntax (documented @ main function).
+ *      ->  Supported functions are conversion between number systems/radices and finding the next number, given the radix.
+ *      ->  The conversion functions contain escapes for bad inputs, thus they can be used for input sanitization by self-converting.
+ * 
+ *      ___Input__Requirements___
+ *      ->  Inputted number should be a non-negative integer for all number systems, unsigned for binary.
+ *      ->  Inputted number should not exceed ULLONG_MAX (2^65 - 1).
  */
 
 
@@ -9,6 +18,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>
+#include <time.h>
 
 
 enum Radix {
@@ -434,4 +444,259 @@ char *nextNumber(char *number, enum Radix radix) {
     }
 
     return res;
+}
+
+
+int main(int argc, char *argv[]) {
+    /**
+     *      ___Command__Line__Argument__Format___
+     *          .\radix.exe <function-flag> [radix-flag] <number> [conversion-flag]
+     * 
+     *      <> - Required by syntax
+     *      [] - Option
+     * 
+     *      ___Function__Flags___
+     *      Necessary argument
+     *      -c / -convert : Calls conversion function
+     *      -n / -next    : Calls sequence function nextNumber()
+     * 
+     *      ___Radix__Flags___
+     *      OPTION - If omitted, radix is assumed to be decimal by default
+     *      -b / -bin / -binary      :  Sets input radix as binary
+     *      -d / -dec / -decimal     :  Sets input radix as decimal
+     *      -h / -hex / -hexadecimal :  Sets input radix as hexadecimal
+     *      -o / -oct / -octal       :  Sets input radix as octal
+     * 
+     *      ___Number___
+     *      Numeral string - Necessary argument
+     * 
+     *      ___Conversion__Flags___
+     *      OPTION - If omitted, radix is assumed to be set input radix by default
+     *      Same options as radix flags
+     *      Only used for conversion functions, will be ignored in sequence function (as extra parameters)
+     */
+
+
+    if (argc < 3) {
+        printf("Invalid Syntax - check docstring for command line syntax\n");
+        return -1;  // Returns -1, signalling insufficient arguments
+    }
+
+
+    int x = 2, y;
+    char *input, *output;       // Pointers for sanitized input and output strings respectively
+    char function;              // Function flag - stores either c or n
+    enum Radix 
+        inputRadix = DECIMAL,   // Input radix flag - stores input number's radix
+        conversionRadix;        // Conversion radix flag - stores radix for conversion, if needed
+    clock_t startTime, endTime;
+
+
+    startTime = clock();
+
+
+    if (
+        strcmp(argv[1], "-c") == 0 || 
+        strcmp(argv[1], "-convert") == 0
+    ) { function = 'c'; }
+
+    else if (
+        strcmp(argv[1], "-n") == 0 || 
+        strcmp(argv[1], "-next") == 0
+    ) { function = 'n'; } 
+    
+    else {
+        printf("Invalid Syntax - check docstring for command line syntax\n");
+        return 1;   // Returns index of bad argument
+    }
+
+
+    if (
+        strcmp(argv[x], "-b") == 0 || 
+        strcmp(argv[x], "-bin") == 0 || 
+        strcmp(argv[x], "-binary") == 0
+    ) { 
+        inputRadix = BINARY;
+        conversionRadix = BINARY;
+        ++x;
+    }   
+    else if (
+        strcmp(argv[x], "-d") == 0 || 
+        strcmp(argv[x], "-dec") == 0 || 
+        strcmp(argv[x], "-decimal") == 0
+    ) {
+        conversionRadix = DECIMAL;
+        ++x;
+    }    
+    else if (
+        strcmp(argv[x], "-h") == 0 || 
+        strcmp(argv[x], "-hex") == 0 || 
+        strcmp(argv[x], "-hexadecimal") == 0
+    ) {
+        inputRadix = HEXADECIMAL;
+        conversionRadix = HEXADECIMAL;
+        ++x;
+    }  
+    else if (
+        strcmp(argv[x], "-o") == 0 || 
+        strcmp(argv[x], "-oct") == 0 || 
+        strcmp(argv[x], "-octal") == 0
+    ) {
+        inputRadix = OCTAL;
+        conversionRadix = OCTAL;
+        ++x;
+    }
+
+
+    switch(inputRadix) {
+        case BINARY:
+            input = convertBinary(argv[x], BINARY);
+        break;
+
+        case DECIMAL:
+            input = convertDecimal(argv[x], DECIMAL);
+        break;
+
+        case HEXADECIMAL:
+            input = convertHexadecimal(argv[x], HEXADECIMAL);
+        break;
+
+        case OCTAL:
+            input = convertOctal(argv[x], OCTAL);
+        break;
+    }
+
+
+    if (input == NULL) return x;    // Returns index of input string
+
+    switch(function) {
+        case 'c':
+            y = x + 1;
+            if (argc > y) {
+                if (
+                    strcmp(argv[y], "-b") == 0 || 
+                    strcmp(argv[y], "-bin") == 0 || 
+                    strcmp(argv[y], "-binary") == 0
+                ) { conversionRadix = BINARY; } 
+
+                else if (
+                    strcmp(argv[y], "-d") == 0 || 
+                    strcmp(argv[y], "-dec") == 0 || 
+                    strcmp(argv[y], "-decimal") == 0
+                ) { conversionRadix = DECIMAL; } 
+                
+                else if (
+                    strcmp(argv[y], "-h") == 0 || 
+                    strcmp(argv[y], "-hex") == 0 || 
+                    strcmp(argv[y], "-hexadecimal") == 0
+                ) { conversionRadix = HEXADECIMAL; } 
+                
+                else if (
+                    strcmp(argv[y], "-o") == 0 || 
+                    strcmp(argv[y], "-oct") == 0 || 
+                    strcmp(argv[y], "-octal") == 0
+                ) { conversionRadix = OCTAL; } 
+                
+                else --y;
+            }
+
+            switch(inputRadix) {
+                case BINARY:
+                    output = convertBinary(input, conversionRadix);
+                break;
+
+                case DECIMAL:
+                    output = convertDecimal(input, conversionRadix);
+                break;
+
+                case HEXADECIMAL:
+                    output = convertHexadecimal(input, conversionRadix);
+                break;
+
+                case OCTAL:
+                    output = convertOctal(input, conversionRadix);
+                break;
+            }
+        break;
+
+        case 'n':
+            output = nextNumber(input, inputRadix);
+            if (output == NULL) return x;   // Returns index of bad number
+            y = x;
+        break;
+    }
+
+    if (argc > y + 1) {
+        printf("WARNING: Extra arguments will be ignored...\n\n");
+    }
+
+    switch(function) {
+        case 'c':
+            switch(inputRadix) {
+                case BINARY:
+                    printf("Binary Number:\t\t%s\n", input);
+                break;
+
+                case DECIMAL:
+                    printf("Decimal Number:\t\t%s\n", input);
+                break;
+
+                case HEXADECIMAL:
+                    printf("Hexadecimal Number:\t%s\n", input);
+                break;
+
+                case OCTAL:
+                    printf("Octal Number:\t\t%s\n", input);
+                break;
+            }
+
+            switch(conversionRadix) {
+                case BINARY:
+                    printf("Binary Number:\t\t%s\n", output);
+                break;
+
+                case DECIMAL:
+                    printf("Decimal Number:\t\t%s\n", output);
+                break;
+
+                case HEXADECIMAL:
+                    printf("Hexadecimal Number:\t%s\n", output);
+                break;
+
+                case OCTAL:
+                    printf("Octal Number:\t\t%s\n", output);
+                break;
+            }
+        break;
+
+        case 'n':
+            switch(inputRadix) {
+                case BINARY:
+                    printf("Next Binary Number:\t\t%s\n", output);
+                break;
+
+                case DECIMAL:
+                    printf("Next Decimal Number:\t\t%s\n", output);
+                break;
+
+                case HEXADECIMAL:
+                    printf("Next Hexadecimal Number:\t%s\n", output);
+                break;
+
+                case OCTAL:
+                    printf("Next Octal Number:\t\t%s\n", output);
+                break;
+            }
+        break;
+    }
+
+    endTime = clock();
+
+    double timeUsed = ((double) (endTime - startTime)) / CLOCKS_PER_SEC;
+    printf("\nExecution time: %.4lf seconds", timeUsed);
+
+    free(input);
+    free(output);
+
+    return 0;
 }
