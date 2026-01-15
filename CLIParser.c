e343769ff844c5fb8153a4e71d4bfbd7add28606
Kevin <kevinpremkumar23@gmail.com>
2026-01-16 01:03:54 +0530

    Restructuring project - initial commit with new clipboard functions, inputter module and radix.c rewritten as header library

---
+++ CLIParser.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "ClipboardFunctions.h"
+#include "Inputter.h"
+
+
+/**
+ *      ____________Command__Line__Syntax____________
+ *      (argv[0]) <mode> <option> [option-parameters]
+ * 
+ *      <> - necessary arguments
+ *      [] - optional parameters
+ * 
+ * 
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |      MODE      |    OPTION     |        PARAMS          |                        DESCRIPTION                        |
+ *      |----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a normal counting run with binary numbers.      |
+ *      |     binary     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |   d / dec / decimal    | Converts an inputted binary number to decimal.            |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |  h / hex / hexadecimal | Converts an inputted binary number to hexadecimal.        |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |               |     o / oct / octal    | Converts an inputted binary number to octal.              |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a normal counting run with decimal numbers.     |
+ *      | decimal/normal |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |    b / bin / binary    | Converts an inputted decimal number to octal.             |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |  h / hex / hexadecimal | Converts an inputted decimal number to hexadecimal.       |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |               |     o / oct / octal    | Converts an inputted decimal number to octal.             |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a normal counting run with emojified numbers.   |
+ *      |      emoji     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |          NIL           | Converts an inputted number into an emojified number.     |
+ *      |                |  emojify / e  |                        |                                                           |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a normal counting run with hexadecimal numbers. |
+ *      |   hexadecimal  |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |    b / bin / binary    | Converts an inputted hexadecimal number to binary.        |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |    d / dec / decimal   | Converts an inputted hexadecimal number to decimal.       |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |               |     o / oct / octal    | Converts an inputted hexadecimal number to octal.         |   
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using numbers with digits in     |
+ *      |   increasing   |               |                        | non-decreasing order.                                     |
+ *      |                |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |   check / c   |          NIL           | Checks whether an inputted number's digits are in         |
+ *      |                |               |                        | non-decreasing order.                                     |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using Japanese numbers.          |
+ *      |    japanese    |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |          NIL           | Translates an inputted number to Japanese, and converts   |
+ *      |                | translate / t |                        | an inputted Japanese number to its decimal form.          |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a normal counting run with octal numbers.       |
+ *      |      octal     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |    b / bin / binary    | Converts an inputted octal number to binary.              |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |    d / dec / decimal   | Converts an inputted octal number to decimal.             |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |               |  h / hex / hexadecimal | Converts an inputted octal number to hexadecimal.         |   
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |          69            | Initiates a counting run using '69' numbers.              |
+ *      |      meme      |               |------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          420           | Initiates a counting run using '420' numbers.             |
+ *      |                |               |------------------------+-----------------------------------------------------------+
+ *      |                |               |         69420          | Initiates a counting run using both '69' numbers and '420'|
+ *      |                |               |                        | numbers.                                                  |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run in Morse code.                   |
+ *      |      morse     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |          NIL           | Converts an inputted Morse code to its numerical value,   |
+ *      |                |               |                        | and an inputted number to Morse code.                     |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using numbers that have no       |
+ *      |      norep     |               |                        | recurring digits.                                         |
+ *      |                |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |   check / c   |          NIL           | Checks whether an inputted number contains no recurring   |
+ *      |                |               |                        | digits.                                                   |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |       nwn      |    run / r    |          NIL           | Initiates a counting run using Numbers within Numbers.    |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |      nwnwn     |    run / r    |          NIL           | Initiates a counting run using Numbers within Numbers     |
+ *      |                |               |                        | within Numbers.                                           |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |     nwnwnn     |    run / r    |          NIL           | Initiates a counting run using Numbers within Numbers     |
+ *      |                |               |                        | within N Numbers.                                         |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using palindromic numbers.       |
+ *      |   palindrome   |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |   check / c   |          NIL           | Checks whether an inputted number is a palindrome.        |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using numbers that have at least |
+ *      |       rep      |               |                        | one repeating digit.                                      |
+ *      |                |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |   check / c   |          NIL           | Checks whether an inputted number contains at least one   |
+ *      |                |               |                        | repeating digit.                                          |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run with Roman Numerals.             |
+ *      |      roman     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |  convert / c  |          NIL           | Converts an inputted Roman number to its numerical value, |
+ *      |                |               |                        | and an inputted number to its Roman numeral equivalent.   |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ *      |                |    run / r    |          NIL           | Initiates a counting run using tally marks.               |
+ *      |      tally     |---------------+------------------------+-----------------------------------------------------------+
+ *      |                |               |                        | Converts an inputted number to its tally mark form        |
+ *      |                |  convert / c  |          NIL           | (incorporating tally mark strikethrough according to      |
+ *      |                |               |                        | Discord markdown rules), and counts tally marks to assess |
+ *      |                |               |                        | its true numerical value.                                 |
+ *      +----------------+---------------+------------------------+-----------------------------------------------------------+
+ */
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) 
+    {
+        printf("Invalid Syntax: %s <mode> <option> [option-parameters]\n\nPlease review documentation for more information on the command line syntax...");
+        return 1;
+    }
+
+    char option;
+    char param[4] = {0};
+
+    create_temp_file();
+
+    if (strcmp(argv[1], "binary") == 0)
+    {
+        await_user_input();
+        // Read file input
+        // Call CLI as a process
+    }
+}
---
+++ radix.h
@@ -0,0 +1,149 @@
+/**
+ *      Number Systems
+ *      Utilities for conversions among number systems
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+#include <errno.h>
+
+
+enum Radix
+{
+    BINARY = 2,
+    OCTAL = 8,
+    DECIMAL = 10,
+    HEXADECIMAL = 16
+};
+
+// Function mapping radix to its name
+const char *radix_name(enum Radix radix) 
+{
+    switch(radix)
+    {
+        case BINARY: return "binary";
+        case DECIMAL: return "decimal";
+        case HEXADECIMAL: return "hexadecimal";
+        case OCTAL: return "octal";
+        default: return "unknown-base";
+    }
+}
+
+
+// Helper function
+void number_to_string(char *, unsigned long long, enum Radix);
+
+// NOTE: Self-conversion can be used for input sanitization.
+char *convert(char *, enum Radix, enum Radix);
+
+// Sequence Function - Get next number in sequence by specifying sanitized input with radix of the number
+char *next_number(char *, enum Radix);
+
+
+//  For inputted binary strings, ensure string length doesn't exceed 64 + 1
+//  For inputted decimal strings, ensure string length doesn't exceed 20 + 1
+//  For inputted hexadecimal strings, ensure string length doesn't exceed 16 + 1
+//  For inputted octal strings, ensure string length doesn't exceed 22 + 1
+
+//  BORDERLINE CASES
+//  For inputted decimal strings of length 20 + 1, ensure string does not exceed 18446744073709551615
+//  For inputted octal strings of length 22 + 1, ensure string does not exceed 1777777777777777777777
+
+
+void number_to_string(char *buffer, unsigned long long number, enum Radix base)
+{
+    switch(base) 
+    {
+        case BINARY:
+            int k = 0;
+
+            // Construct string in reverse
+            do
+            {
+                buffer[k++] = '0' + (number & 1);
+                number >>= 1;
+            }
+            while (number != 0);
+            
+            buffer[k] = '\0';
+
+            // Reverse final order to correct string
+            for (
+                int left = 0, right = k - 1; 
+                left < right; 
+                left++, right--
+            ) {
+                char temp = buffer[left];
+                buffer[left] = buffer[right];
+                buffer[right] = temp;
+            }
+        break;
+
+        case DECIMAL:
+            sprintf(buffer, "%llu", number);
+        break;
+
+        case HEXADECIMAL:
+            sprintf(buffer, "%llX", number);
+        break;
+
+        case OCTAL:
+            sprintf(buffer, "%llo", number);
+        break;
+    }
+}
+
+
+char *convert(char *number, enum Radix convertFrom, enum Radix convertTo)
+{
+    unsigned long long value;
+    char *numString, *endChar;
+    errno = 0;
+
+    if (number[0] == '-')
+    {
+        printf("Bad Input: Negative numbers not allowed");
+        return NULL;
+    }
+
+    value = strtoull(number, &endChar, convertFrom);
+
+    if (*endChar != '\0')
+    {
+        printf("Bad Input: Invalid %s number", radix_name(convertFrom));
+        return NULL;
+    }
+
+    if (errno == ERANGE)
+    {
+        printf("Bad Input: Number too large");
+        return NULL;
+    }
+
+    numString = (char *)malloc(65 * sizeof(char));
+    number_to_string(numString, value, convertTo);
+
+    return numString;
+}
+
+
+char *next_number(char *number, enum Radix base)
+{
+    // Ensure input is already sanitized
+    char *nextNumber;
+    unsigned long long value = strtoull(number, NULL, base);    // Error check not necessary since input is already sanitized
+    
+    if (value == ULLONG_MAX)  // Subtle detail: value increments here
+    {
+        printf("Bad Input - Maximum allowed number");
+        return NULL;
+    }
+
+    nextNumber = (char *)malloc(65 * sizeof(char));
+    number_to_string(nextNumber, value + 1, base);
+
+    return nextNumber;
+}
