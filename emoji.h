b2e4dc2e57fb00b66a28869a76b8d7ba48f96b3d
Kevin <kevinpremkumar23@gmail.com>
2026-01-18 00:07:59 +0530

    Updated emoji module as a header file

---
+++ emoji.h
@@ -0,0 +1,61 @@
+/**
+ *      Emoji
+ *      Functions to emojify numbers
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+const char *EMOJI_DIGITS[10] = 
+{
+    "0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣",
+    "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"
+};
+
+
+// Emojifies number, only accepts inputs upto 1 billion
+char *numberToEmoji(char *number)
+{
+    int n = strlen(number);
+    if (number[0] == '-')
+    {
+        printf("Bad Input - Negative numbers not allowed");
+        return NULL;
+    }
+
+    char *res = (char *)malloc(64 * sizeof(char));
+    if (!res)
+    {
+        printf("Error - malloc failure");
+        return NULL;
+    }
+    res[0] = '\n';
+
+    for (int i = 0; number[i] != '\0'; i++) 
+    {
+        // Catch invalid inputs
+        if (!isdigit(number[i]))
+        {
+            printf("Bad Input - Invalid number");
+            free(res);
+            return NULL;
+        }
+
+        // Catch extra large numbers
+        // Only supports numbers < 1 billion
+        if (i >= 9)
+        {
+            printf("Bad Input - Number too large");
+            free(res);
+            return NULL;
+        }
+
+        strcat(res, EMOJI_DIGITS[number[i]]);
+    }
+
+    return res;
+}
