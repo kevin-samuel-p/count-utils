72fa4b4282a76ddeeeb05ad61105acdf3df63eff
Kevin <kevinpremkumar23@gmail.com>
2026-03-11 22:44:59 +0530

    Restructure macOS build shell script to prevent writes to system volume

--- packaging/mac/build_pkg.sh
+++ packaging/mac/build_pkg.sh
@@ -6,23 +6,23 @@ VERSION="1.0.0"
 
 PKG_DIR="packaging/mac"
 
-mkdir -p $PKG_DIR/usr/local/bin
-mkdir -p $PKG_DIR/usr/local/share/count/docs
+mkdir -p $PKG_DIR/bin
+mkdir -p $PKG_DIR/share/count/docs
 
-rm -rf $PKG_DIR/usr/local/bin/*
-rm -rf $PKG_DIR/usr/local/share/count/docs/*
+rm -rf $PKG_DIR/bin/*
+rm -rf $PKG_DIR/share/count/docs/*
 
 # copy binary
-cp build/mac/count $PKG_DIR/usr/local/bin/
+cp build/mac/count $PKG_DIR/bin/
 
 # copy docs
-cp docs/* $PKG_DIR/usr/local/share/count/docs/
+cp docs/* $PKG_DIR/share/count/docs/
 
 pkgbuild \
   --root $PKG_DIR \
   --identifier com.count.cli \
   --version $VERSION \
-  --install-location / \
+  --install-location /usr/local \
   $PKG_DIR/${APP_NAME}_${VERSION}.pkg
 
 echo "macOS package created:"
